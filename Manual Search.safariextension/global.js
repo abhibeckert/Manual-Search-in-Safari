580e5b74706a007616fcc65282b22388790f8eec
Abhi Beckert <abhibeckert@gmail.com>
2013-06-25 12:40:55 +1000

    no-longer try to load domain names with whitespace

--- Manual Search.safariextension/global.js
+++ Manual Search.safariextension/global.js
@@ -9,6 +9,9 @@ function handleBeforeSearch(e)
   var url;
   if (e.query.match(/^g /)) { // do we want a google search?
     url = 'http://google.com/search?q=' + e.query.replace(/^g +/, '');
+  
+  } else if (e.query.match(/^[^\/]* /)) { // there is a space before the first / (or a space and no /) do a google search
+    url = 'http://google.com/search?q=' + e.query;
     
   } else { // no google search, just slap http:// in front of it
     url = 'http://' + e.query;
